2d69c0f5bb4cf0edceb3e449d7f22f898059a310
karim el assali <fifakarim52@gmail.com>
2025-12-20 16:43:45 +0100

    feat: implement locale-aware home page with dynamic components and maintenance mode

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -40,7 +40,7 @@ export default async function Home() {
       <Hero />
       <About />
       {/* <CategoriesSection /> */}
-      <PaymentMethodsB ox />
+      <PaymentMethodsBox />
       <OffersSection />
       <GallerySection />
       <ReviewsSection />
